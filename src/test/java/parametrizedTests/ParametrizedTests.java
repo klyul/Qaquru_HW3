28da057b534f9006a52b9b2b1a801d662f805ce4
klyul <yuliya.v3333@gmail.com>
2023-03-26 13:59:33 +0300

    HW9

--- src/test/java/parametrizedTests/ParametrizedTests.java
+++ src/test/java/parametrizedTests/ParametrizedTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.RegistrationPage;
 import pages.components.RegistrationResultsModal;
@@ -20,9 +22,7 @@ public class ParametrizedTests extends TestBase {
             "Москва, ул.Ангелов д 5", "75 PARK PLACE 8TH FLOOR NEW YORK NY 10007"
     })
     @Tag("Regress")
-    @DisplayName("Корректное отображение адреса введенного при регистрации в модальном окне подтверждения")
-    @ParameterizedTest
-    @Test
+    @ParameterizedTest(name = "Корректное отображение адреса введенного при регистрации в модальном окне подтверждения")
     void fillFormTest() {
 
         registrationPage.openPage()
@@ -56,5 +56,84 @@ public class ParametrizedTests extends TestBase {
                         .substring(studentData.getFilePath().lastIndexOf("/")+ 1));
 
     }
+
+
+    @Tag("Regress")
+    @ParameterizedTest(name = "Тест формы с гендером {0}")
+    @CsvSource(value = {
+            "Male", "Female", "Other"
+    })
+    void fillFormTestGenders(String genderParameter) {
+
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(studentData.getFirstName())
+                .setLastName(studentData.getLastName())
+                .setEmail(studentData.getUserEmail())
+                .setGender(genderParameter)
+                .setPhone(studentData.getUserPhone())
+                .setBirthDate(studentData.getDay(), studentData.getMonth(), studentData.getYear())
+                .setSubjects(studentData.getSubject())
+                .setHobby(studentData.getHobby())
+                .setPicture(studentData.getFilePath())
+                .setAddress(studentData.getAddress())
+                .setState(studentData.getState())
+                .setCity(studentData.getCity())
+                .clickSubmit();
+
+
+        registrationResultsModal.verifyModalAppears()
+                .verifyResult("Student Name", studentData.getFirstName() + " " + studentData.getLastName())
+                .verifyResult("Student Email", studentData.getUserEmail())
+                .verifyResult("Gender", genderParameter)
+                .verifyResult("Mobile", studentData.getUserPhone())
+                .verifyResult("Date of Birth", studentData.getDay() + " " + studentData.getMonth() + "," + studentData.getYear())
+                .verifyResult("Subjects", studentData.getSubject())
+                .verifyResult("Hobbies", studentData.getHobby())
+                .verifyResult("Address", studentData.getAddress())
+                .verifyResult("State and City", studentData.getState() + " " + studentData.getCity())
+                .verifyResult("Picture", studentData.getFilePath()
+                        .substring(studentData.getFilePath().lastIndexOf("/")+ 1));
+
+    }
+
+    @Tag("Regress")
+    @ParameterizedTest(name = "Тест формы со штатом {0} и городом {1}")
+    @CsvFileSource(resources = "/statesCities.csv")
+    void fillFormTestStateCity(String state, String city) {
+
+        registrationPage.openPage()
+                .removeBanner()
+                .setFirstName(studentData.getFirstName())
+                .setLastName(studentData.getLastName())
+                .setEmail(studentData.getUserEmail())
+                .setGender(studentData.getGender())
+                .setPhone(studentData.getUserPhone())
+                .setBirthDate(studentData.getDay(), studentData.getMonth(), studentData.getYear())
+                .setSubjects(studentData.getSubject())
+                .setHobby(studentData.getHobby())
+                .setPicture(studentData.getFilePath())
+                .setAddress(studentData.getAddress())
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
+
+
+        registrationResultsModal.verifyModalAppears()
+                .verifyResult("Student Name", studentData.getFirstName() + " " + studentData.getLastName())
+                .verifyResult("Student Email", studentData.getUserEmail())
+                .verifyResult("Gender", studentData.getGender())
+                .verifyResult("Mobile", studentData.getUserPhone())
+                .verifyResult("Date of Birth", studentData.getDay() + " " + studentData.getMonth() + "," + studentData.getYear())
+                .verifyResult("Subjects", studentData.getSubject())
+                .verifyResult("Hobbies", studentData.getHobby())
+                .verifyResult("Address", studentData.getAddress())
+                .verifyResult("State and City", state + " " + city)
+                .verifyResult("Picture", studentData.getFilePath()
+                        .substring(studentData.getFilePath().lastIndexOf("/")+ 1));
+
+    }
+
+
 }
 
