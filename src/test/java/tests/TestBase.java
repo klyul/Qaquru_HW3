671b89815292a9492f78f4e5e4adc0b266e65d5d
klyul <yuliya.v3333@gmail.com>
2023-04-08 23:09:35 +0300

    hw12

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,62 @@
-package tests;
-
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class TestBase {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    private static String executionMode;
+
     @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        // Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        executionMode = System.getProperty("executionMode", "local");
+        logger.info("Execution mode: " + executionMode);
+
+        if (!System.getProperty("os.name").startsWith("Windows")) {
+            executionMode = "remote";
+        }
+
+        if (executionMode.equals("remote")) {
+            Configuration.browser = System.getProperty("browser", "chrome");
+            logger.info("Browser" + Configuration.browser);
+
+            Configuration.browserVersion = System.getProperty("browserVersion", "100");
+            logger.info("Browser version" + Configuration.browserVersion);
+
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            Configuration.browserCapabilities = capabilities;
+        }
+
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        if (executionMode.equals("remote")) {
+            Attach.addVideo();
+        }
+    }
+
 }
